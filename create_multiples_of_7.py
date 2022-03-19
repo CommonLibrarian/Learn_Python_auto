890cf3d2526230f8b3a2ace2083bdc059d59b5dc
Eugene Lee <70938784+CommonLibrarian@users.noreply.github.com>
2022-03-19 09:23:56 +0900

    Update create_multiples_of_7.py

--- create_multiples_of_7.py
+++ create_multiples_of_7.py
@@ -1,5 +1,13 @@
+#create multiples of seven by FOR LOOP
 multiples = []
 for x in range(1,11):
   multiples.append(x*7)
   
- print(multiples)
+print(multiples)
+
+#alternative
+multiples = [] #empty matrix
+for x in range(1,11)
+  multiples.append(x*7)
+  
+print(multiples)
