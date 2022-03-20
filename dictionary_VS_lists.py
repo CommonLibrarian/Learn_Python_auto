1bb185d78302c66928e3954af5fdfb57f4768c6c
Eugene Lee <70938784+CommonLibrarian@users.noreply.github.com>
2022-03-20 14:21:06 +0900

    Create dictionary_VS_lists.py

---
+++ dictionary_VS_lists.py
@@ -0,0 +1,6 @@
+wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
+for products, colors in wardrobe.items():
+	for any_colors in colors:
+		print("{} {}".format(any_colors, products))
+    
+    
